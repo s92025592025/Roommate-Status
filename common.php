08427b309f2b9c32ecf07d17dda772b3c9b7caa0
s92025592025 <s92025592025@gmail.com>
2016-09-21 14:43:15 -0700

    fits mobile devices better

--- common.php
+++ common.php
@@ -4,6 +4,7 @@
 			<!DOCTYPE html>
 				<html>
 					<head>
+					<meta name="viewport" content="width=device-width, initial-scale=1.0">
 					<!--JQuery-->
 					<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 					<!--Bootstrap-->
