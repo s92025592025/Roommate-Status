565a42ef79f4a2e15320f6cb474a426765093940
s92025592025 <s92025592025@gmail.com>
2016-09-21 14:55:00 -0700

    login basic layout finished

--- login.php
+++ login.php
@@ -1,12 +1,32 @@
 <?php
-	session_start();
+	include "common.php";
 
-	if(isset($_SESSION["login"]) && $_SESSION["login"] == "TRUE"){
-		header("Location: editStatus.php");
-		die();
-	}
+	if($_SERVER["REQUEST_METHOD"] != "POST"){
+		session_start();
 
-	HTMLHeader("Login", "", "");
+		if(isset($_SESSION["login"]) && $_SESSION["login"] == "TRUE"){
+			header("Location: editStatus.php");
+			die();
+		}
 
-	HTMLFooter();
+		HTMLHeader("Login", "login.css", "");
+		?>
+			<div class="row">
+				<div class="col-sm-offset-4 col-sm-4">
+					<form class="form-horizontal" action="login.php" method="POST">
+						<div class="form-group">
+							<label class="control-label">Passcode: </label>
+							<div>
+								<input type="password" class="form-control" id="passcode">
+							</div>
+						</div>
+						<div class="form-group">
+							<button class="btn btn-default" type="submit">Login</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		<?php
+		HTMLFooter();
+	}else{}
 ?>
